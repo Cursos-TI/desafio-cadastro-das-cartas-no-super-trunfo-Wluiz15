08c3b91687951a741eb483a9650e98215904e64d
Wluiz15 <wesley15luiz@gmail.com>
2025-08-18 14:18:38 +0000

    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,9 @@ int main() {
     int pontosTuristicos1, pontosTuristicos2;
     float area1, area2;
     float pib1, pib2;
+    float densidade1, densidade2;
+    float pibPerCapita1, pibPerCapita2;
+    
 
     printf(" Bem-vindo ao jogo Super Trunfo de Cidades! \n");
 
@@ -38,20 +41,43 @@ int main() {
 
     printf("\n    Dados coletados com sucesso!   \n");
 
+    //Soma da densidade populacional 
+    densidade1 = (populacao1 / area1);
+    densidade2 = (populacao2 / area2);
+
+    //Soma da PIB per capita
+    pibPerCapita1 = (pib1 / populacao1);
+    pibPerCapita2 = (pib2 / populacao2);
+
+    
+
     // Exibição dos dados
     printf("\nResumo das cidades:\n");
 
+    // Cidade 1
     printf("\nCidade 1:\n");
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
-    printf("PIB: R$ %.2f bilhões\n", pib1);
+    printf("PIB: R$ %.2f\n", pib1);
     printf("Pontos turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade populacional: %.2f\n", densidade1);
+    printf("PIB Per capita: %.2f\n", pibPerCapita1);
 
+    // Cidade 2
     printf("\nCidade 2:\n");
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: R$ %.2f bilhões\n", pib2);
+    printf("PIB: R$ %.2f\n", pib2);
     printf("Pontos turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade populacional: %.2f\n", densidade2);
+    printf("PIB Per capita: %.2f\n", pibPerCapita2);
+
+
+
+
+
+
+
 
     return 0;
 }
